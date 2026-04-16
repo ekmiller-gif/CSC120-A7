b702062c1c3cdfaed7b1b583c889a6cf6b253821
ekmiller <ekmiller@smith.edu>
2026-04-16 13:35:59 -0400

    Completed House class and addes attributes and constructor to Cafe class

--- Cafe.java
+++ Cafe.java
@@ -1,12 +1,31 @@
 /* This is a stub for the Cafe class */
-public class Cafe {
+public class Cafe extends Building implements CafeRequirements{
 
-    public Cafe() {
+    private int nCoffeeOunces; 
+    private int nSugarPackets; 
+    private int nCreams; 
+    private int nCups; 
+
+    /** 
+     * @param name name of cafe
+     * @param address address of cafe
+     * @param nFloors number of floors in cafe
+     * @param nCoffeeOunces ounces of coffee in inventory
+     * @param nSugarPackets number of sugar packets in inventory
+     * @param nCreams number of servings of cream in inventory
+     * @param nCups number of cups in inventory
+    */
+    public Cafe(String name, String address, int nFloors) {
+        super(name, address, nFloors);
+        this.nCoffeeOunces = nCoffeeOunces;
+        this.nSugarPackets = nSugarPackets;
+        this.nCreams = nCreams;
+        this.nCups = nCups;
         System.out.println("You have built a cafe: ☕");
     }
     
     public static void main(String[] args) {
-        new Cafe();
+        Cafe cc = new Cafe("CC Cafe", "1 Chapin Way", 1);
     }
     
 }
--- House.java
+++ House.java
@@ -1,12 +1,84 @@
 /* This is a stub for the House class */
-public class House {
 
-  public House() {
-    System.out.println("You have built a house: 🏠");
+import java.util.ArrayList;
+
+public class House extends Building implements HouseRequirements{
+
+  //house attributes
+  private boolean hasDiningRoom;
+  private ArrayList<Student> residents;
+
+  /**
+     * House constructor
+     * @param hasDiningRoom Does the House have a dining room?
+     * @param residents students in house
+     */
+    public House(String name, String address, int nFloors, boolean hasDiningRoom){
+        super(name, address, nFloors);
+        this.hasDiningRoom = hasDiningRoom;
+        this.residents = new ArrayList<Student>();
+        System.out.println("You have built a house: 🏠");
   }
+    
+
+
+  /**
+  * Accessor for hasDiningRoom
+  * @return hasDiningRoom() true/false if house has a dining room
+  */
+  public boolean hasDiningRoom(){
+    return this.hasDiningRoom;
+  }
+
+  /**
+  * Accessor for residents
+  * @return residents.size() number of residents in house
+  */
+  public int nResidents(){
+    return residents.size();
+  }
+
+  /**
+  * Adds a student to the list of residents
+  */
+  public void moveIn(Student s){
+    residents.add(s);
+  }
+
+  /**
+  * Removes a student from the list of residents and returns the student removed
+  * @return s student that has moved out
+  */
+  public Student moveOut(Student s){
+    residents.remove(s);
+    return s;
+  }
+
+  /**
+  * Indicates if a student lives in the house
+  * @return boolean true/false if student is a resident of the house
+  */
+  public boolean isResident(Student s){
+    if (residents.contains(s)){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  
+  
+
+    
 
   public static void main(String[] args) {
-    new House();
+    House gardiner = new House("Gardiner", "1 Paradise Rd", 4, false);
+    Student em = new Student("emmarie", "999", 2026);
+    Student de = new Student("devin", "997", 2026);
+    gardiner.moveIn(em);
+    gardiner.moveIn(de);
+    System.out.println(gardiner.residents.contains(de));
   }
+
 
 }
